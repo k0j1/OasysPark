b57e2c2275ce7b9079e602ab887a676ba8fa89a3
k0j1 <k0j123t@gmail.com>
2024-06-29 14:35:03 +0900

    【拡張機能】Ver.0.1.2.10 ヒーロー及びエクステンションにマウスホバー時にステータス表示対応
    ・イベントリにて、ヒーロー及びエクステンションにマウスホバー時にステータス表示するように対応
    ・イベントリデータ変更時を検知してマウスホバー動作を埋め込む様に変更

--- 拡張機能/MCHスカウター/popup/scouter.js
+++ 拡張機能/MCHスカウター/popup/scouter.js
@@ -19,6 +19,10 @@ overlayElem.innerHTML = `
         <div class="left-arrow2"></div>      
     </div>
 `;
+// var scriptElem = document.createElement("script");
+// scriptElem.src = "https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js";
+// document.head.appendChild(scriptElem);
+
 var styleElem = document.createElement("style");
 	styleElem.textContent = `
 		.overlay { position: absolute; width: 124px; height: 124px; background-color: rgba(128,128,128,0.0); z-index: 2; padding:0; }
@@ -132,6 +136,13 @@ var styleElem = document.createElement("style");
                 width: 25%;
             }
         }
+
+        .chart-status{
+            position:absolute;
+            width:100%;
+            height:100%;
+        }
+        
 	`;
 document.head.appendChild(styleElem);
 
@@ -141,37 +152,66 @@ function resetTargetClass(target){
 }
 
 var preTarget = null;
+// イベントリの読み込み完了したら、ホバー時のアクションセット
 var fnCheckInventory = function() {
     var elemItems = document.getElementsByClassName("assetSelectableListItem__inner");
     if(0 < elemItems.length){
         //elemItems[1].classList.add("fedein");    
         for(elem of elemItems){
             elem.addEventListener("mouseover", (event) => {
-                if(preTarget != event.currentTarget){
-                    overlayElem.className = "overlay";
-                    overlayElem.classList.add("fedein");
-                    event.currentTarget.prepend(overlayElem);                        
+                let overElems = event.currentTarget.getElementsByClassName("overlay");
+                if(overElems.length == 0){
+                    // ホバー時の動作
+                    if(preTarget != event.currentTarget){
+                        overlayElem.className = "overlay";
+                        overlayElem.classList.add("fedein");
+                        event.currentTarget.prepend(overlayElem);
+                    }
+                    if(window.location.href.includes("/heroes/")){
+                        // ヒーロー情報取得
+                        const heroID = getID(event.currentTarget);
+                        getRequestHeroInfo(event.currentTarget, heroID);
+                    }else if(window.location.href.includes("/extensions/")){
+                        // エクステンション情報取得
+                        const extID = getID(event.currentTarget);
+                        getRequestExtInfo(event.currentTarget, extID);
+                    }
+                    preTarget = event.currentTarget;
+                }else{
+
                 }
-                preTarget = event.currentTarget;
             });
             // elem.addEventListener("mouseleave", (event) =>{
             //     setTimeout(resetTargetClass, 3000, event.currentTarget);
             // });
-            setTimeout(fnCheckChangeURL,1000);
+            //setTimeout(fnCheckEmptyAsset,1000);
         }
     }else{
         setTimeout(fnCheckInventory,1000);
     }
 };
 
-var fnCheckChangeURL = function() {
-    var elemItems = document.getElementsByClassName("assetSelectableListItem__inner");
-    if(0 == elemItems.length){
-        setTimeout(fnCheckInventory,1000);
-    }else{
-        setTimeout(fnCheckChangeURL,1000);
-    }
-};
+const observer = new MutationObserver(() => {
+    // ここにDOM変更時の処理を書く
+    setTimeout(fnCheckInventory,1000);
+    console.log('変更を検知');
+});
+
+observer.observe(document.body, {
+    subtree: true,
+    childList: true, 
+    attributes: true,
+    characterData: true
+});
+// // アイテムが空になったらイベントリにアクションセット開始
+// var fnCheckEmptyAsset = function() {
+//     var elemItems = document.getElementsByClassName("assetSelectableListItem__inner");
+//     if(0 == elemItems.length){
+//         setTimeout(fnCheckInventory,1000);
+//     }else{
+//         setTimeout(fnCheckEmptyAsset,1000);
+//     }
+// };
 
 // popstate イベントは、ブラウザのアクション（例えば、戻るボタンや進むボタン）によってアクティブな履歴エントリが変更されたときに発生
 // window.addEventListener('popstate', function(event) {
@@ -179,4 +219,108 @@ var fnCheckChangeURL = function() {
 //     // ここに処理を記述
 //     setTimeout(fn,1000);
 // });
+function setChart(parentElem, id, hp, phy, int, agi){
+    //var elemItems = document.getElementsByClassName("assetSelectableListItem__inner");
+    var canvElem = document.createElement("canvas");
+    canvElem.id = "chart_" + id;
+    canvElem.className = "chart-status overlay";
+    var chart = new Chart(canvElem, {
+        type: 'pie',
+        data: {
+            label: 'ステータス',
+            //labels: ["HP", "PHY", "INT", "AGI"],
+            datasets: [{
+                data:[hp,phy,int,agi],
+                backgroundColor: [
+                    'rgba(255,206,86,0.3)',
+                    'rgba(255,99,132,0.3)',
+                    'rgba(153,102,255,0.3)',
+                    'rgba(75,192,192,0.3)'
+                ],
+                borderColor: [
+                    'rgba(255,206,86,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(153,102,255,1)',
+                    'rgba(75,192,192,1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            
+        }
+    });
+    parentElem.prepend(canvElem);
+    parentElem.removeChild(overlayElem);
+}
 
+function getID(target){
+    const classNameForHeroID = "assetSelectableListItem__basic__id";
+    const elemIDs = target.getElementsByClassName(classNameForHeroID);
+    if(0 >= elemIDs.length) return 0;
+    console.log("getID:" + elemIDs[0].textContent);
+    return elemIDs[0].textContent;
+}
+
+// ヒーローIDを元にURLからjson取得
+function getRequestHeroInfo(target, heroID)
+{
+	// ヒーローID一覧取得
+    const urlHeroIDURL = "https://www.mycryptoheroes.net/metadata/hero/" + heroID.replace("#","");
+	console.log(urlHeroIDURL)
+	let request = new XMLHttpRequest();
+	try {
+		request.open('GET', urlHeroIDURL);
+		request.responseType = 'json';
+		request.onreadystatechange = function() {
+            console.log("request.readyState="+request.readyState);
+            console.log("request.status="+request.status);
+            if(request.readyState==4 && request.status==0){
+                //DispList(null, "heroID_", "❌所持ヒーロー", data, reqHeroInfoURL);
+                console.log("getRequestHeroInfo error1");
+            }
+		}
+		request.send();
+	} catch (e) {
+        console.log("getRequestHeroInfo error2");
+	}
+	request.onload = function()
+	{
+		const heroInfo = request.response;
+		if(heroInfo && heroInfo.attributes)
+		{
+            setChart(target, heroID, heroInfo.attributes.hp, heroInfo.attributes.phy, heroInfo.attributes.int, heroInfo.attributes.agi);
+        }else{
+            console.log("getRequestHeroInfo error3");
+		}
+	};
+	request.onerror = function(){
+        console.log("getRequestHeroInfo error4");
+	}
+}
+function getRequestExtInfo(target, extID)
+{
+	// エクステンションID一覧取得
+    const urlExtIDURL = "https://www.mycryptoheroes.net/metadata/extension/" + extID.replace("#","");
+	console.log(urlExtIDURL)
+	let requestExt = new XMLHttpRequest();
+	try {
+		requestExt.open('GET', urlExtIDURL);
+		requestExt.responseType = 'json';
+		requestExt.send();
+	} catch (e) {
+		console.log("getRequestExtInfo error1");
+	}
+	requestExt.onload = function() {
+		const extInfo = requestExt.response;
+		if(extInfo && extInfo.attributes)
+		{
+            setChart(target, extID, extInfo.attributes.hp, extInfo.attributes.phy, extInfo.attributes.int, extInfo.attributes.agi);
+        }else{
+            console.log("getRequestExtInfo error2");
+		}
+	};
+	requestExt.onerror = function(){
+		console.log("getRequestExtInfo error3");
+	}
+}
